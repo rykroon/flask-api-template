cf8792c9a2aa988c768e4286390ce07644b69bab
Ryan Kroon <rykroon.tech@gmail.com>
2020-08-03 23:13:47 -0400

    created a get_redis_client() method

--- services/api/src/db.py
+++ services/api/src/db.py
@@ -1,6 +1,6 @@
 import os
 from mongoengine import connect
-from redis import Redis
+import redis
 
 
 mongo_host = os.getenv('MONGODB_HOST')
@@ -8,4 +8,9 @@ connect('test_db', host=mongo_host)
 
 redis_host = os.getenv('REDIS_HOST')
 redis_pass = os.getenv('REDIS_PASSWORD')
-redis_client = Redis(host=redis_host, password=redis_pass, db=0)
+redis_connection_pool = redis.ConnectionPool(host=redis_host, password=redis_pass, db=0)
+
+def get_redis_client():
+    return redis.Redis(connection_pool=redis_connection_pool)
+
+redis_client = get_redis_client()
