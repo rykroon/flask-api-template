0614e2b5edfb9ae85d392656b8b909a8e8748087
Ryan Kroon <rykroon.tech@gmail.com>
2020-08-10 23:27:03 -0400

    renamed _prepend_key_prefix to make_key

--- services/api/src/core/caches.py
+++ services/api/src/core/caches.py
@@ -11,10 +11,10 @@ class Cache:
         self.default_timeout = timeout
 
     def __contains__(self, key):
-        return self._prepend_key_prefix(key) in redis_client
+        return self.make_key(key) in redis_client
 
     def get(self, key, default=None):
-        value = redis_client.get(self._prepend_key_prefix(key))
+        value = redis_client.get(self.make_key(key))
         if value is None:
             return default
         return self._load_object(value)
@@ -22,15 +22,15 @@ class Cache:
     def set(self, key, value, timeout=None):
         timeout = timeout or self.default_timeout
         value = self._dump_object(value)
-        return redis_client.set(self._prepend_key_prefix(key), value, ex=timeout)
+        return redis_client.set(self.make_key(key), value, ex=timeout)
 
     def add(self, key, value, timeout=None):
         timeout = timeout or self.default_timeout
         value = self._dump_object(value)
-        return redis_client.set(self._prepend_key_prefix(key), value, ex=timeout, nx=True)
+        return redis_client.set(self.make_key(key), value, ex=timeout, nx=True)
 
     def delete(self, key):
-        return redis_client.delete(self._prepend_key_prefix(key)) == 1
+        return redis_client.delete(self.make_key(key)) == 1
 
     def _dump_object(self, value):
         return pickle.dumps(value)
@@ -44,7 +44,7 @@ class Cache:
         except pickle.UnpicklingError:
             return value
 
-    def _prepend_key_prefix(self, key):
+    def make_key(self, key):
         if self.key_prefix:
             return '{}:{}'.format(self.key_prefix, key)
         return key
