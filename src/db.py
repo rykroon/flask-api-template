d5a71f5aff2aac58097d64f4a321300ab18713c5
Ryan Kroon <rykroon.tech@gmail.com>
2020-11-16 21:08:51 -0500

    added MONGO_DB, MONGO_HOST, and MONGO_PORT env vars

--- src/db.py
+++ src/db.py
@@ -1,5 +1,11 @@
 import os
+from mongoengine import connect
 import redis
+
+mongo_db = os.getenv('MONGO_DB')
+mongo_host = os.getenv('MONGO_HOST', 'localhost')
+mongo_port = os.getenv('MONGO_PORT', 27017)
+connect(mongo_db, host=mongo_host, port=mongo_port)
 
 redis_host = os.getenv('REDIS_HOST')
 redis_pass = os.getenv('REDIS_PASSWORD')
