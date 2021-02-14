eb90b36c2f4a8fd66c416de378d53dc40edb0b06
Ryan Kroon <rykroon.tech@gmail.com>
2021-02-14 11:25:06 -0500

    refactored Cache class

--- src/utils/cache.py
+++ src/utils/cache.py
@@ -2,17 +2,23 @@ from functools import wraps
 import pickle
 from flask import g, request
 
+# use 'Undefined' to differentiate between 'None' and 
+# the absense of passing in a value.
+Undefined = object()
+
 
 class Cache:
-    def __init__(self, key_prefix=None, timeout=300):
+    def __init__(self, key_prefix=None, timeout=Undefined):
         self.key_prefix = key_prefix
-        self.default_timeout = timeout
+        self.default_timeout = 300 if timeout is Undefined else timeout
 
     def __contains__(self, key):
-        return self._make_key(key) in g.redis_client
+        key = self._make_key(key)
+        return key in g.redis_client
 
     def get(self, key, default=None):
-        value = g.redis_client.get(self._make_key(key))
+        key = self._make_key(key)
+        value = g.redis_client.get(key)
         if value is None:
             return default
         
@@ -21,13 +27,20 @@ class Cache:
         except pickle.UnpicklingError:
             return value
 
-    def set(self, key, value, timeout=None):
-        timeout = timeout or self.default_timeout
+    def set(self, key, value, timeout=Undefined):
+        key = self._make_key(key)
         value = pickle.dumps(value)
-        return g.redis_client.set(self._make_key(key), value, ex=timeout)
+        timeout = self.default_timeout if timeout is Undefined else timeout
+        return g.redis_client.set(key, value, ex=timeout)
+
+    def touch(self, key, timeout=Undefined):
+        key = self._make_key(key)
+        timeout = self.default_timeout if timeout is Undefined else timeout
+        return g.redis_client.expire(key, timeout)
 
     def delete(self, key):
-        return g.redis_client.delete(self._make_key(key)) == 1
+        key = self._make_key(key)
+        return g.redis_client.delete(key) == 1
 
     def _make_key(self, key):
         if self.key_prefix:
