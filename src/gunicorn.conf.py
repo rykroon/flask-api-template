c89c56db90135bd073556381282ebf589a1ebca3
Ryan Kroon <rykroon.tech@gmail.com>
2021-02-19 15:06:04 -0500

    added gunicorn.conf.py file

---
+++ src/gunicorn.conf.py
@@ -0,0 +1,13 @@
+import os
+
+
+bind = os.getenv('GUNICORN_BIND', '0.0.0.0')
+
+loglevel = os.getenv('GUNICORN_LOGLEVEL', 'info')
+
+reload = os.getenv('GUNICORN_RELOAD', False)
+
+workers = os.getenv('GUNICORN_WORKERS', 2 * os.cpu_count() + 1)
+
+worker_type = os.getenv('GUNICORN_WORKER_TYPE', 'gevent')
+
