12b74c82fad0564e91388ac725a786346c94050e
Ryan Kroon <rykroon.tech@gmail.com>
2021-02-27 18:50:54 -0500

    refactoring throttling code

--- src/rest/throttling.py
+++ src/rest/throttling.py
@@ -2,56 +2,52 @@ from functools import wraps
 from math import ceil
 import time
 from flask import g, request
-from werkzeug.exceptions import TooManyRequests
 from utils import Cache
 
 
-def throttle(rate, scope='global'):  
-    def decorator(func):
-        @wraps(func)
-        def wrapper(*args, **kwargs):
-            throttler = Throttler(rate, scope)
-            throttler.throttle()
-            return func(*args, **kwargs)
-        return wrapper
-    return decorator
-
-
-class Throttler:
-    def __init__(self, rate, scope='global'):
+class Throttle:
+    def __init__(self, scope, rate):
+        """
+            :param scope: can be either 'anon' or 'user'
+            :param rate: {num_requests}/{period} 
+        """
         self.scope = scope
         self.num_of_requests, self.duration = self.parse_rate(rate)
         self.cache = Cache(key_prefix='throttle', timeout=self.duration)
 
-    @property
-    def key(self):
-        if 'client' in g:
-            ident = g.client.pk
-        elif request.access_route:
-            ident = request.access_route[0]
-        else:
-            ident = request.remote_addr
+    def get_cache_key(self):
+        ident = None
+        if self.scope == 'anon':
+            ident = request.remote_addr or '127.0.0.1'
 
-        return '{}:{}'.format(self.scope, ident)
+        if self.scope == 'user':
+            if g.client:
+                ident = g.client.pk
+
+        if ident is not None:
+            return '{}:{}'.format(self.scope, ident)
 
     def parse_rate(self, rate):
-        num, _, period = rate.partition('/')
-        num_requests = int(num)
+        num_requests, _, period = rate.partition('/')
+        num_requests = int(num_requests)
         duration = {'s': 1, 'm': 60, 'h': 3600, 'd': 86400}[period[0]]
         return (num_requests, duration)
 
-    def throttle(self):
-        history = self.cache.get(self.key, [])
+    def allow_request(self):
+        key = self.get_cache_key()
+        if key is None:
+            return True
+
+        history = self.cache.get(key, [])
         now = time.time()
         while history and history[-1] <= now - self.duration:
             history.pop()
 
         if len(history) >= self.num_of_requests:
-            retry_after = ceil(self.duration - (now - history[-1]))
-            raise TooManyRequests(retry_after=retry_after)
+            return False
 
         history.insert(0, now)
-        self.cache.set(self.key, history, self.duration)
+        self.cache.set(key, history, self.duration)
         return True
 
  
