f5ef7a82c737f0963426c29a29410a494e9c21ae
Ryan Kroon <rykroon.tech@gmail.com>
2022-01-08 13:02:29 -0500

    Additional logic to BaseModel.

--- app/models/base.py
+++ app/models/base.py
@@ -1,10 +1,22 @@
 from datetime import datetime
+import uuid
 
 from mongoengine import Document
 from mongoengine.fields import DateTimeField, UUIDField
 
 class BaseDocument(Document):
 
-    uuid = UUIDField()
-    date_created = DateTimeField()
-    date_updated = DateTimeField()
+    meta = {
+        'abstract': True,
+        'allow_inheritance': True
+    }
+
+    uuid = UUIDField(binary=False, required=True, default=uuid.uuid4)
+    date_created = DateTimeField(default=datetime.utcnow)
+    date_updated = DateTimeField(null=True)
+
+    def clean(self):
+        super().clean()
+
+        if self._created or self._get_changed_fields():
+            self.date_updated = datetime.utcnow()
