3c00579c5ef4e1b5b9d1f56342bc1b09dfa237dc
Ryan Kroon <rykroon.tech@gmail.com>
2022-06-03 21:26:43 -0400

    Removed unused code.

--- app/views/api/__init__.py
+++ app/views/api/__init__.py
@@ -2,7 +2,6 @@ from flask import Blueprint
 
 from utils.auth import BasicAuthentication
 from utils.middleware import AuthenticationMiddleware
-from views.api.tokens import bp as tokens_bp
 
 
 bp = Blueprint('api', __name__, url_prefix='/api')
@@ -13,6 +12,3 @@ def auth_middleware():
     AuthenticationMiddleware(
         authentication_classes=[BasicAuthentication]
     )()
-
-
-bp.register_blueprint(tokens_bp)
